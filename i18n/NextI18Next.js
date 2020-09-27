97bd773c18cb0b5f117a60b705df22e5830d4d1c
Mateusz Walendzik <mate.walendzik@gmail.com>
2020-09-27 21:47:18 +0200

    upgrade i18next, remove custom server.js script (#4)

--- i18n/NextI18Next.js
+++ i18n/NextI18Next.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const NextI18Next = require('next-i18next').default
+const path = require('path')
+// const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 
 module.exports = new NextI18Next({
   defaultLanguage: 'en',
   otherLanguages: ['pl'],
+  // localeSubpaths,
+  localePath: path.resolve('./public/static/locales'),
 })
